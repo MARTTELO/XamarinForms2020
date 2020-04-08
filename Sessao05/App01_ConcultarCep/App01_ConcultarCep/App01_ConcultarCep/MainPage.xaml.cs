324314c3a1c859770d3c62800040b23e0b466ebd
Fatec <marttelo@gmail.com>
2020-04-08 00:18:16 -0300

    Implementando classes e metodos para acesso VIACEP

--- Sessao05/App01_ConcultarCep/App01_ConcultarCep/App01_ConcultarCep/MainPage.xaml.cs
+++ Sessao05/App01_ConcultarCep/App01_ConcultarCep/App01_ConcultarCep/MainPage.xaml.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using App01_ConcultarCep.Servico.Modelo;
+using App01_ConcultarCep.Servico;
+
 
 namespace App01_ConcultarCep
 {
@@ -16,6 +19,62 @@ namespace App01_ConcultarCep
         public MainPage()
         {
             InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            //To do - Logica do Programa
+            string cep = CEP.Text.Trim();
+
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+                    if (end != null)
+                    {
+                        RESULTADO.Text = string.Format("Endereço: {0}, {1}/{2}", end.logradouro, end.localidade, end.uf);
+                    }
+                    else
+                    {
+                        DisplayAlert("Erro", "Não foi encontrado endereço pro CEP informado: " + cep,"OK");
+                    }
+                   
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("ERRO CRÍTICO:", ex.Message, "OK");
+                   
+                }
+                
+            }
+      
+
+        }
+
+        private bool isValidCEP(string cep)
+        {
+
+            bool valido = true;
+
+            //if(cep.Length != 8)
+            //{
+            //    //ERRO
+            //    DisplayAlert("Erro", "Cep inválido! O Cep deve ter 8 caracteres","OK");
+            //    valido = false;
+            //}
+        
+            int NovoCEP = 0;
+
+            if(!int.TryParse(cep, out NovoCEP))
+            {
+                DisplayAlert("Erro", "Cep inválido! O Cep deve ser numérico", "OK");
+                valido = false;
+            }
+            return valido;
+
         }
     }
 }
---
+++ Sessao05/App01_ConcultarCep/App01_ConcultarCep/App01_ConcultarCep/Servico/ViaCEPServico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App01_ConcultarCep.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_ConcultarCep.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoUrl = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoUrl = string.Format(EnderecoUrl, cep);
+
+            System.Net.WebClient wc = new System.Net.WebClient();
+
+            string Conteudo = wc.DownloadString(NovoEnderecoUrl);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            if (end.cep == null) return null;
+           
+            return end;
+        }
+    }
+}
