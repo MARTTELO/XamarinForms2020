2c26e3d1f08f2cf647cb5c3506d40ad12823d221
Marcelo Ludwig Junior <marttelo@outlook.com>
2020-07-27 16:10:34 -0300

    criando pastas, modelos e paginas

---
+++ Sessao12/App1_Mimica/App1/App1/Model/Grupo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Model
+{
+    public class Grupo
+    {
+        public string Nome { get; set; }
+        public short Pontuacao { get; set; }
+    }
+}
